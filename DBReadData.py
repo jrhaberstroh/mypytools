0f5360927832e39d67e335539e0f010cbdb95d63
John Haberstroh <jhaberstroh@berkeley.edu>
2013-06-19 18:33:57 -0700

    Updated DB*Data.py, consolidated into dbwrap.py

--- DBReadData.py
+++ DBReadData.py
@@ -55,7 +55,64 @@ def PrintDBCols():
 	for ind,col in enumerate(cols[0]):
 		if (cols[2][ind] != None):
 			print "%d " % ind + col + " " + cols[1][ind] + "; DEFAULT = " + str(cols[2][ind])
+
+
+def Read(*args, **kwargs):
+	"""
+	Calls, schematically speaking, 'SELECT *args FROM table_name WHERE **kwargs'
+	Pass *args for the desired columns.
+	Pass **kwargs to specify WHERE options, in the simple format 'key == value'. Value can be a tuple, in which case an "or" is placed between tuple options.
+
+	1) Checks that all **kwarg keys are in the database as columns.
+	2) Formats **kwarg arguments into database properly.
+	3) Checks that all *args values are in the database as columns.
+
+	Note that table_name is still insecure.
+	"""
+	if db_path == None or table_name == None:
+		print "You must set BOTH the table_name and the db_path namespace variables to run Read()!"
+		raise ValueError
+
+	all_cols = GetDBCols()[0];
+	#all_cols = ['Col1', 'shoebox']
+	for arg_col in args:
+		if not any(col == arg_col for col in all_cols):
+			print "Column "+arc_col+" supplied by *args not valid"
+			raise ValueError
+	column_str = ', '.join(args)
 	
+	constraints_and = []
+	constraints_val_list = []
+	for key, val_list in kwargs.iteritems():
+		constraints_or = []
+		if any(col == key for col in all_cols):
+			if type(val_list) != tuple and type(val_list) != list:
+				# If we were not passed a tuple or list, make a tuple so that our object is properly iterable.
+				val_list = (val_list,)
+			for val in val_list:
+				constraints_or.append(key + "==?")
+				constraints_val_list.append(val)
+			constraints_and.append('(' + " OR ".join(constraints_or) + ')')
+
+		else:
+			print "Column "+key+" supplied by **kwargs not valid"
+			raise ValueError
+	constraints = ' AND '.join(constraints_and)
+	
+	command =  "SELECT " + column_str + " FROM " + table_name + " WHERE " + constraints
+	print command
+	print constraints_val_list
+
+	conn = sqlite3.connect(db_path)
+	c = conn.cursor()
+	x = c.execute(command, constraints_val_list)
+	query = c.fetchall()
+	conn.close()
+
+	return query
 
 
 
+#args = ['Col1', 'shoebox']
+#kwargs = {'Col1': (1, 2, 23), 'shoebox' : 10}
+#Read(*args, **kwargs)
--- dbwrap.py
+++ dbwrap.py
@@ -6,7 +6,8 @@ db_path = None
 table_name = None
 
 """
-Module DBAddData:
+Module dbwrap:
+	Merger of DBAddData and DBReadData
 
 A light wrapper for adding data to a sqlite database using python dicts.
 Variable db_path: sets the path to the desired database.
@@ -17,7 +18,12 @@ GetDBCols() is available to avoid typographical errors.
 AddToDB(**kwargs) is the function to add data to the database.
 """
 
-def GetDBCols():
+def CheckTable():
+	if db_path == None or table_name == None:
+		print "You must set BOTH the table_name and the db_path namespace variables to run Read()!"
+		raise ValueError
+
+def GetCols():
 	"""
 	Reads the database table (table_name) from db_path and returns a list:
 		0) Column names (unicode)
@@ -26,6 +32,8 @@ def GetDBCols():
 	NOTE: table_name string formatting NOT secure...
 	Note that for default values, python will appropriately read the NULL character as None, independent of any .sqliterc settings.
 	"""
+	CheckTable()
+
 	#arg = table_name,
 	conn = sqlite3.connect(db_path)
 	c = conn.cursor()
@@ -43,11 +51,12 @@ def GetDBCols():
 
 	return [cols, types, req]
 
-def PrintDBCols():
+def PrintCols():
 	"""
 	Prints the database columns in a user friendly way, and specifies which entries are required vs optional.
 	Also presents the user with the indexes for better using GetDBCols().
 	"""
+	CheckTable()
 
 	print "PrintDBCols():"
 	print "NOTE: Call GetDBCols to get the raw data."
@@ -65,7 +74,7 @@ def PrintDBCols():
 		if (cols[2][ind] != None):
 			print "%d " % ind + col + " " + cols[1][ind] + "; DEFAULT = " + str(cols[2][ind])
 	
-def AddToDB(**kwargs):
+def AddData(**kwargs):
 	"""
 	Setup local variables db_path and table_name first.
 	Pass required kwargs & corresponding data, as specified by PrintDBCols(), as key::value pairs.
@@ -77,6 +86,7 @@ def AddToDB(**kwargs):
 	It can raise any excpetions that sqlite3 can raise.
 	It returns the command to SQL and the argument list passed to c.execute().
 	"""
+	CheckTable()
 
 	all_cols = GetDBCols()[0];
 	db_args = []
@@ -104,7 +114,60 @@ def AddToDB(**kwargs):
 	conn.close() 
 	return [command, arg_list]
 
+
+def ReadData(*args, **kwargs):
+	"""
+	Calls, schematically speaking, 'SELECT *args FROM table_name WHERE **kwargs'
+	Pass *args for the desired columns.
+	Pass **kwargs to specify WHERE options, in the simple format 'key == value'. Value can be a tuple, in which case an "or" is placed between tuple options.
+
+	1) Checks that all **kwarg keys are in the database as columns.
+	2) Formats **kwarg arguments into database properly.
+	3) Checks that all *args values are in the database as columns.
+
+	Note that table_name is still insecure.
+	"""
+	CheckTable()
+
+	all_cols = GetDBCols()[0];
+	#all_cols = ['Col1', 'shoebox']
+	for arg_col in args:
+		if not any(col == arg_col for col in all_cols):
+			print "Column "+arc_col+" supplied by *args not valid"
+			raise ValueError
+	column_str = ', '.join(args)
+	
+	constraints_and = []
+	constraints_val_list = []
+	for key, val_list in kwargs.iteritems():
+		constraints_or = []
+		if any(col == key for col in all_cols):
+			if type(val_list) != tuple and type(val_list) != list:
+				# If we were not passed a tuple or list, make a tuple so that our object is properly iterable.
+				val_list = (val_list,)
+			for val in val_list:
+				constraints_or.append(key + "==?")
+				constraints_val_list.append(val)
+			constraints_and.append('(' + " OR ".join(constraints_or) + ')')
+
+		else:
+			print "Column "+key+" supplied by **kwargs not valid"
+			raise ValueError
+	constraints = ' AND '.join(constraints_and)
+	
+	command =  "SELECT " + column_str + " FROM " + table_name + " WHERE " + constraints
+	print command
+	print constraints_val_list
+
+	conn = sqlite3.connect(db_path)
+	c = conn.cursor()
+	x = c.execute(command, constraints_val_list)
+	query = c.fetchall()
+	conn.close()
+	return query
+
+
+
 def test():
 	## test requires a test database...
-	#GetDBCols();
 	return True
